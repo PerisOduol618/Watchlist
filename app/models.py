54b9d89a87b15beabc49e7d0744e462f2e2b3a9b
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-19 17:59:43 +0300

    add all changes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+
+    
+
+
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -11,7 +20,7 @@ class Movie:
         self.vote_average = vote_average
         self.vote_count = vote_count
 
-
+    
 
 class Review:
 
@@ -42,3 +51,53 @@ class Review:
                 response.append(review)
 
         return response
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    password_secure = db.Column(db.String(255))
+
+    def is_active(self):
+        return True
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+  
+  
+
+       
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the attribute')
+
+    @set_password.setter
+    def password(self,password):
+        self.password_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_secure,password)
+
+
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+
+    
+    def __repr__(self):
+        return f'User {self.name}'
+    
+
+    
--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@ export MOVIE_API_KEY='0a77c28db33a50c32bedb42e685e7859'
 
 export SECRET_KEY="FlaskSecret"
 
+export PYTHONDONTWRITEBYTECODE=1
+
 python3.6 manage.py server
